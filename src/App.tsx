9091faa143aa0482f7ecfac09805dd3060e3372a
Kesavarapu Vishnuvardhan Reddy <vishnuvardhanmec@gmail.com>
2025-11-18 22:44:38 +0000

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import "./App.css";
 
 import { Header } from "./components/header";
 import { Hero } from "./components/hero";
-import { ImageGallery } from "./components/ImageGallery";
 import TempleRoutes from "./components/temple-routes";
 import { Contact } from "./components/contact";
 import { Services } from "./components/services";
@@ -14,7 +13,6 @@ function App() {
       <Header />
       <Hero />
       <Services />
-      <ImageGallery />
        <TempleRoutes />
       <Contact />
       <Footer />
